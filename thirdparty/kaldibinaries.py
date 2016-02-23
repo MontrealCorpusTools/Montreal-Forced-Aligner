f8e4395992c9428d0d548ad7f4947ad4508815e6
michaelasocolof <michaelasocolof@wpa160019.wireless.mcgill.ca>
2016-02-23 16:56:31 -0500

    Fixed binary collecting

--- thirdparty/kaldibinaries.py
+++ thirdparty/kaldibinaries.py
@@ -1,15 +1,18 @@
+import shutil, os
+import argparse
+
 def CollectBinaries(directory):
-	import shutil, os
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument('dir')
-	args = parser.parse_args()
-	directory = args.dir
+	outdirectory = os.path.dirname(os.path.realpath(__file__))
 	for root, dirs, files in os.walk(directory):
 		for name in files:
 			ext = os.path.splitext(name)
 			(key, value) = ext
 			if value == '' and key != '.DS_Store' and key != 'configure' and key != 'Doxyfile' and key != 'INSTALL' and key != 'NOTES' and key != 'TODO' and key != 'Makefile':
-				shutil.copy(root + '/' + name, os.getcwd())
+				shutil.copy(root + '/' + name, outdirectory)
 
-CollectBinaries('directory')
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('dir')
+	args = parser.parse_args()
+	directory = args.dir
+	CollectBinaries(directory)
