9cb52e6414032717c209bf965feea1566f8d0b5c
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-05-15 21:16:51 -0400

    Build fixes

--- freezing/hooks/hook-aligner.py
+++ freezing/hooks/hook-aligner.py
@@ -5,6 +5,9 @@ import sys
 from PyInstaller.utils.hooks import (
     collect_data_files, collect_dynamic_libs)
 
+hiddenimports=['six','packaging', 'packaging.version', 'packaging.specifiers',
+            'packaging.requirements', 'scipy', 'scipy.linalg']
+
 thirdparty_dir = os.path.abspath(os.path.join('thirdparty','bin'))
 binaries = [( os.path.join(thirdparty_dir, x), 'thirdparty/bin') for x in os.listdir(thirdparty_dir)
     ]
