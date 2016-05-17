c367f83104de828abc3be465f826673e2bc9c0f8
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-05-17 15:35:48 -0400

    Update build script

--- freezing/freeze.sh
+++ freezing/freeze.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+if [ `uname` == Darwin ]; then
 
-pyinstaller -F --clean -n montreal-forced-aligner -y --additional-hooks-dir=freezing/hooks aligner/command_line/train_and_align.py
+    pyinstaller -w --clean -n montreal-forced-aligner -y --additional-hooks-dir=freezing/hooks aligner/command_line/train_and_align.py
+
+else
+
+    pyinstaller -F --clean -n montreal-forced-aligner -y --additional-hooks-dir=freezing/hooks aligner/command_line/train_and_align.py
+
+fi
