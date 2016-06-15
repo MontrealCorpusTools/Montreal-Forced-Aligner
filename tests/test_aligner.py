3f4e8f598be1b508cbb4dd1c94785a7987575135
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-06-15 12:17:34 -0400

    Fix imports

--- tests/test_aligner.py
+++ tests/test_aligner.py
@@ -1,22 +1,22 @@
 import os
 import pytest
 
-from aligner.aligner import BaseAligner
+from aligner.aligner import TrainableAligner
 
 def test_sick_mono(sick_dict, sick_corpus, generated_dir):
-    a = BaseAligner(sick_corpus, sick_dict, os.path.join(generated_dir,'sick_output'),
+    a = TrainableAligner(sick_corpus, sick_dict, os.path.join(generated_dir,'sick_output'),
                         temp_directory = os.path.join(generated_dir,'sickcorpus'))
     a.train_mono()
 
 @pytest.mark.xfail
 def test_sick_tri(sick_dict, sick_corpus, generated_dir):
-    a = BaseAligner(sick_corpus, sick_dict, os.path.join(generated_dir,'sick_output'),
+    a = TrainableAligner(sick_corpus, sick_dict, os.path.join(generated_dir,'sick_output'),
                         temp_directory = os.path.join(generated_dir,'sickcorpus'))
     a.train_tri()
 
 @pytest.mark.xfail
 def test_sick_tri_fmllr(sick_dict, sick_corpus, generated_dir):
-    a = BaseAligner(sick_corpus, sick_dict, os.path.join(generated_dir,'sick_output'),
+    a = TrainableAligner(sick_corpus, sick_dict, os.path.join(generated_dir,'sick_output'),
                         temp_directory = os.path.join(generated_dir,'sickcorpus'))
     a.train_tri_fmllr()
     a.export_textgrids()
