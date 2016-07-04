55f454b5e27598e29fb114d88cc751f2c838e76d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-07-04 14:25:56 -0400

    Update freezing

--- freezing/freeze.bat
+++ freezing/freeze.bat
@@ -1,9 +1,11 @@
 
 pyinstaller --clean -y ^
 --additional-hooks-dir=freezing/hooks ^
+--exclude-module tkinter ^
 aligner/command_line/train_and_align.py
 
 pyinstaller --clean -y ^
 --additional-hooks-dir=freezing/hooks ^
+--exclude-module tkinter ^
 aligner/command_line/align.py
 
--- freezing/freeze.sh
+++ freezing/freeze.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
 
-pyinstaller --clean -y --additional-hooks-dir=freezing/hooks aligner/command_line/train_and_align.py
+rm -rf dist
 
-pyinstaller --clean -y --additional-hooks-dir=freezing/hooks aligner/command_line/align.py
+pyinstaller --clean -y \
+--additional-hooks-dir=freezing/hooks \
+--exclude-module tkinter \
+--exclude-module matplotlib \
+--exclude-module pytz \
+--exclude-module sphinx \
+ aligner/command_line/train_and_align.py
+
+pyinstaller --clean -y \
+--additional-hooks-dir=freezing/hooks \
+--exclude-module tkinter \
+--exclude-module matplotlib \
+--exclude-module pytz \
+--exclude-module sphinx \
+aligner/command_line/align.py
+
+cd dist
+mkdir montreal-forced-aligner
+
+mv train_and_align/ montreal-forced-aligner/lib
+mv align/align montreal-forced-aligner/lib/align
+
+cd montreal-forced-aligner
+mkdir bin
+cd bin
+ln -s ../lib/train_and_align train_and_align
+ln -s ../lib/align align
+
+cd ../..
+zip -r montreal-forced-aligner.zip montreal-forced-aligner/
+
+cd ..
