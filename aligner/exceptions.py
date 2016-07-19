23132c8fcfa8424bc0112137decb96dec66811d7
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-07-19 14:22:55 -0400

    Some docstrings, WIP

--- aligner/exceptions.py
+++ aligner/exceptions.py
@@ -1,25 +1,43 @@
 
 
 class MFAError(Exception):
+    '''
+    Base exception class
+    '''
     pass
 
 ## Dictionary Errors
 
 class DictionaryError(MFAError):
+    '''
+    Class for errors in creating Dictionary objects
+    '''
     pass
 
 ## Corpus Errors
 
 class CorpusError(MFAError):
+    '''
+    Class for errors in creating Corpus objects
+    '''
     pass
 
 class SampleRateError(CorpusError):
+    '''
+    Class for errors in different sample rates
+    '''
     pass
 
 ## Aligner Errors
 
 class AlignerError(MFAError):
+    '''
+    Class for errors during alignment
+    '''
     pass
 
 class NoSuccessfulAlignments(AlignerError):
+    '''
+    Class for errors where nothing could be aligned
+    '''
     pass
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
+import aligner
 
 # -- General configuration ------------------------------------------------
 
@@ -32,8 +33,15 @@
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'numpydoc',
 ]
+autosummary_generate = True
+autodoc_default_flags = ['members']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
