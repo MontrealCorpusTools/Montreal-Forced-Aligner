0eacadd96706b2b61823b1244a72d8681bb95e27
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-07-25 21:45:57 -0400

    Fix up including binaries in Windows releases

--- aligner/helper.py
+++ aligner/helper.py
@@ -1,17 +1,23 @@
 import os
+import sys
 from collections import defaultdict
 
 def thirdparty_binary(binary_name):
-    base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    thirdparty_dir = os.path.join(base_dir, 'thirdparty')
+    if getattr(sys, 'frozen', False) and sys.platform == 'win32':
+        base_dir = os.path.dirname(sys.executable)
+        thirdparty_dir = os.path.join(base_dir, 'thirdparty')
+    else:
+        base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        thirdparty_dir = os.path.join(base_dir, 'thirdparty')
     bin_path = os.path.join(thirdparty_dir, 'bin', binary_name)
+    if sys.platform == 'win32':
+        bin_path += '.exe'
     if not os.path.exists(bin_path):
         return binary_name
     return bin_path
 
 def make_path_safe(path):
     return '"{}"'.format(path)
-
 
 def load_text(path):
     with open(path, 'r', encoding = 'utf8') as f:
