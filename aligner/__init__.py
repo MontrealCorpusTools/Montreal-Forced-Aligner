63c44069cc8846d84610da0db085001e8f72996d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-08-11 14:24:04 -0400

    Version bump for 0.6.1

--- aligner/__init__.py
+++ aligner/__init__.py
@@ -1,6 +1,6 @@
 __ver_major__ = 0
-__ver_minor__ = 5
-__ver_patch__ = 0
+__ver_minor__ = 6
+__ver_patch__ = 1
 __ver_tuple__ = (__ver_major__, __ver_minor__, __ver_patch__)
 __version__ = "%d.%d.%d" % __ver_tuple__
 
