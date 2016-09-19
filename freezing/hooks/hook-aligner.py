52322f28ac1f8aaefb0c9f95f4e399e32590a8d9
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-09-19 14:22:02 -0400

    Mac build updates

--- freezing/hooks/hook-aligner.py
+++ freezing/hooks/hook-aligner.py
@@ -6,10 +6,12 @@ from PyInstaller.utils.hooks import (
     collect_data_files, collect_dynamic_libs)
 
 hiddenimports=['six','packaging', 'packaging.version', 'packaging.specifiers',
-            'packaging.requirements', 'scipy', 'scipy.linalg', 'scipy.integrate']
+            'packaging.requirements']
 
 thirdparty_dir = os.path.abspath(os.path.join('thirdparty','bin'))
-binaries = [( os.path.join(thirdparty_dir, x), 'thirdparty/bin') for x in os.listdir(thirdparty_dir)
+binaries = [( os.path.join(thirdparty_dir, x), 'thirdparty/bin')
+            for x in os.listdir(thirdparty_dir)
+            if not x.endswith('dylib')
     ]
 
 
