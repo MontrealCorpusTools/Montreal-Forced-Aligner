6bcc216ba6b76848634782be3e77de182b6621ce
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-09-19 09:11:30 -0400

    Ignore short segments and errors on MFCC
    Also add test

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -21,11 +21,23 @@ def test_stereo(basic_dict_path, textgrid_directory, generated_dir):
     temp = os.path.join(generated_dir, 'stereo')
     dictionary = Dictionary(basic_dict_path, os.path.join(temp, 'basic'))
     dictionary.write()
-    d = Corpus(textgrid_directory, temp)
+    d = Corpus(os.path.join(textgrid_directory, 'stereo'), temp)
     d.write()
     d.create_mfccs()
     d.setup_splits(dictionary)
     assert(d.get_feat_dim() == '39')
+
+def test_short_segments(textgrid_directory, generated_dir):
+    temp = os.path.join(generated_dir, 'short_segments')
+    corpus = Corpus(os.path.join(textgrid_directory, 'short_segments'), temp)
+    corpus.write()
+    corpus.create_mfccs()
+    assert(len(corpus.feat_mapping.keys()) == 2)
+    assert(len(corpus.utt_speak_mapping.keys()) == 2)
+    assert(len(corpus.speak_utt_mapping.keys()) == 1)
+    assert(len(corpus.text_mapping.keys()) == 2)
+    assert(len(corpus.utt_wav_mapping.keys()) == 1)
+    assert(len(corpus.segments.keys()) == 2)
 
 def test_speaker_groupings(large_prosodylab_format_directory, generated_dir):
     output_directory = os.path.join(generated_dir, 'large')
