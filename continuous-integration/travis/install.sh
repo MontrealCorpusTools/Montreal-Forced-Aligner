90ec788ecbfe1b94deeb821e387842679b64ef5b
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-09-19 10:25:25 -0400

    Add long tests back in

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -38,9 +38,10 @@ else
 fi
 
 if [ ! -d "$HOME/tools/mfa_test_data" ]; then
-  mkdir -p $HOME/downloads
   cd $HOME/tools
   git clone https://github.com/MontrealCorpusTools/mfa_test_data.git
 else
+  cd $HOME/tools/mfa_test_data
+  git pull origin
   echo "Test data already installed."
 fi
