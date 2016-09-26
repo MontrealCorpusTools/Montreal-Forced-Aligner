576a7486f2172f2703185ac349a0630dc5e4677e
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2016-09-26 12:26:30 -0400

    Fix for gauss names in configs

--- freezing/hooks/hook-aligner.py
+++ freezing/hooks/hook-aligner.py
@@ -11,7 +11,7 @@ hiddenimports=['six','packaging', 'packaging.version', 'packaging.specifiers',
 thirdparty_dir = os.path.abspath(os.path.join('thirdparty','bin'))
 binaries = [( os.path.join(thirdparty_dir, x), 'thirdparty/bin')
             for x in os.listdir(thirdparty_dir)
-            if not x.endswith('dylib')
+            if not x.endswith('dylib') and not x.endswith('dll')
     ]
 
 
