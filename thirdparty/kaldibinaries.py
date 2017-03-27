5b0dc8b68e57a448625069d0026d496be79b8d5d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-03-27 17:11:11 -0400

    Fix bug in nodict training

--- thirdparty/kaldibinaries.py
+++ thirdparty/kaldibinaries.py
@@ -14,30 +14,31 @@ else:
     exe_ext = ''
     lib_ext = ['.so', '.so.1']
 
-ignored_filenames = ['.DS_Store', 'configure' , 'Doxyfile' ,
-                'INSTALL', 'NOTES','TODO','Makefile','AUTHORS',
-                'COPYING', 'NEWS', 'README', 'CHANGELOG', 'DISCLAIMER',
-                'log', 'makefile', 'packImageTarFile']
+ignored_filenames = ['.DS_Store', 'configure', 'Doxyfile',
+                     'INSTALL', 'NOTES', 'TODO', 'Makefile', 'AUTHORS',
+                     'COPYING', 'NEWS', 'README', 'CHANGELOG', 'DISCLAIMER',
+                     'log', 'makefile', 'packImageTarFile']
 
 included_filenames = ['compute-mfcc-feats', 'copy-feats', 'gmm-acc-stats-ali',
-                    'compile-train-graphs', 'compile-train-graphs-fsts','align-equal-compiled', 'gmm-acc-stats-ali',
-                    'gmm-align-compiled', 'gmm-boost-silence', 'linear-to-nbest',
-                    'lattice-align-words', 'nbest-to-ctm','lattice-to-phone-lattice',
-                    'acc-tree-stats', 'sum-tree-stats', 'convert-ali',
-                    'weight-silence-post', 'gmm-est-fmllr', 'compose-transforms',
-                    'transform-feats', 'gmm-est', 'gmm-sum-accs', 'gmm-init-mono',
-                    'cluster-phones', 'compile-questions', 'build-tree','gmm-init-model',
-                    'gmm-mixup', 'gmm-info', 'fstcompile', 'fstarcsort', 'fstcopy', 'dot', 'compute-cmvn-stats',
-                    'apply-cmvn', 'add-deltas', 'feat-to-dim', 'subset-feats',
-                    'extract-segments', 'openblas', 'openfst64', 'gmm-latgen-faster',
-                      'draw-tree','fstdraw', 'show-transitions']
+                      'compile-train-graphs', 'compile-train-graphs-fsts', 'align-equal-compiled', 'gmm-acc-stats-ali',
+                      'gmm-align-compiled', 'gmm-boost-silence', 'linear-to-nbest',
+                      'lattice-align-words', 'nbest-to-ctm', 'lattice-to-phone-lattice',
+                      'acc-tree-stats', 'sum-tree-stats', 'convert-ali',
+                      'weight-silence-post', 'gmm-est-fmllr', 'compose-transforms',
+                      'transform-feats', 'gmm-est', 'gmm-sum-accs', 'gmm-init-mono',
+                      'cluster-phones', 'compile-questions', 'build-tree', 'gmm-init-model',
+                      'gmm-mixup', 'gmm-info', 'fstcompile', 'fstarcsort', 'fstcopy', 'dot', 'compute-cmvn-stats',
+                      'apply-cmvn', 'add-deltas', 'feat-to-dim', 'subset-feats',
+                      'extract-segments', 'openblas', 'openfst64', 'gmm-latgen-faster',
+                      'draw-tree', 'fstdraw', 'show-transitions']
 
 dylib_pattern = re.compile(r'\s*(.*)\s+\(')
+
 
 def CollectBinaries(directory):
     outdirectory = os.path.dirname(os.path.realpath(__file__))
     bin_out = os.path.join(outdirectory, 'bin')
-    os.makedirs(bin_out, exist_ok = True)
+    os.makedirs(bin_out, exist_ok=True)
     tools_dir = os.path.join(directory, 'tools')
     for root, dirs, files in os.walk(tools_dir):
         for name in files:
@@ -48,7 +49,8 @@ def CollectBinaries(directory):
                 if not os.path.exists(bin_name):
                     shutil.copy(os.path.join(root, name), bin_out)
                     if sys.platform == 'darwin':
-                        p = subprocess.Popen(['otool', '-L', bin_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                        p = subprocess.Popen(['otool', '-L', bin_name], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                             stderr=subprocess.PIPE)
                         output, err = p.communicate()
                         rc = p.returncode
                         output = output.decode()
@@ -57,8 +59,8 @@ def CollectBinaries(directory):
                             if l.startswith('/usr'):
                                 continue
                             lib = os.path.basename(l)
-                            subprocess.call(['install_name_tool','-change',l, '@loader_path/'+lib, bin_name])
-            elif sys.platform == 'win32' and name in ['openfst64.dll','libopenblas.dll']:
+                            subprocess.call(['install_name_tool', '-change', l, '@loader_path/' + lib, bin_name])
+            elif sys.platform == 'win32' and name in ['openfst64.dll', 'libopenblas.dll']:
                 shutil.copy(os.path.join(root, name), bin_out)
             elif sys.platform != 'win32':
                 c = False
@@ -72,7 +74,7 @@ def CollectBinaries(directory):
         src_dir = directory
     else:
         src_dir = os.path.join(directory, 'src')
-    for root, dirs, files in os.walk(src_dir, followlinks = True):
+    for root, dirs, files in os.walk(src_dir, followlinks=True):
         cur_dir = os.path.basename(root)
         for name in files:
             ext = os.path.splitext(name)
@@ -85,6 +87,7 @@ def CollectBinaries(directory):
                     shutil.copy(os.path.join(root, name), bin_out)
                 elif value == lib_ext:
                     shutil.copy(os.path.join(root, name), bin_out)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
