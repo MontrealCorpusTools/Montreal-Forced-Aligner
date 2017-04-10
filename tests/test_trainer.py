87b75e80646b1da279906177f4a9aae73851798b
esteng <elias@arts84238.arts.mcgill.ca>
2017-04-10 11:25:27 -0400

    new unit test

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -4,4 +4,3 @@ from aligner.g2p_trainer.train import Trainer
 
 def test_training(dict_language, dict_output_path):
     path_to_model = Trainer(dict_language, dict_output_path, False).get_path_to_model()
-
