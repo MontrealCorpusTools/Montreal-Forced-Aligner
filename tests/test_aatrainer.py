5b025dbfe407f54806867d8a64a37f9ae02b6eaf
esteng <elias@arts84238.arts.mcgill.ca>
2017-04-12 11:47:20 -0400

    still fixing

--- tests/test_aatrainer.py
+++ tests/test_aatrainer.py
@@ -2,5 +2,5 @@ import pytest
 import os
 from aligner.g2p_trainer.train import Trainer
 
-def test_training(dict_language, dict_output_path):
-    path_to_model = Trainer(dict_language, dict_output_path, False).get_path_to_model()
+def test_training(dict_model_path, dict_output_path):
+    path_to_model = Trainer(dict_model_path, dict_output_path, False).get_path_to_model()
