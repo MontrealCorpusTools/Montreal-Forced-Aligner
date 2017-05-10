c321b95d9ff14ab5fde32ccce6b48b8053fa21b5
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2017-05-10 16:18:27 -0400

    Different way of packaging thirdparty bins

---
+++ freezing/freeze_final.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import shutil
+
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+orig_dir = os.path.join(root_dir, 'thirdparty', 'bin')
+
+if sys.platform == 'win32':
+    out_dir = os.path.join(root_dir, 'dist', 'montreal-forced-aligner', 'bin', 'thirdparty', 'bin')
+else:
+    out_dir = os.path.join(root_dir, 'dist', 'montreal-forced-aligner', 'lib', 'thirdparty', 'bin')
+
+os.makedirs(out_dir, exist_ok=True)
+
+for f in os.listdir(orig_dir):
+    shutil.copyfile(os.path.join(orig_dir, f), os.path.join(out_dir, f))
