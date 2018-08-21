efb71a841e034b7885bfd03211c2bca7d71804ad
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2018-08-20 22:38:48 -0400

    ensure executables are +x

--- thirdparty/kaldi_binaries.py
+++ thirdparty/kaldi_binaries.py
@@ -1,5 +1,5 @@
 import sys
-import shutil, os
+import shutil, os, stat
 import argparse
 import subprocess
 import re
@@ -75,6 +75,8 @@ def collect_linux_tools_binaries(directory):
             if os.path.exists(out_path) and os.path.getsize(in_path) > os.path.getsize(out_path):
                 continue # Get the smallest file size when multiples exist
             shutil.copyfile(in_path, out_path)
+            st = os.stat(out_path)
+            os.chmod(out_path, st.st_mode | stat.S_IEXEC)
             if sys.platform == 'darwin':
                 p = subprocess.Popen(['otool', '-L', out_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                                      stderr=subprocess.PIPE)
@@ -142,6 +144,8 @@ def collect_kaldi_binaries(directory):
                 if key not in included_filenames:
                     continue
                 shutil.copy(os.path.join(root, name), bin_out)
+                st = os.stat(bin_out)
+                os.chmod(bin_out, st.st_mode | stat.S_IEXEC)
             elif name in included_libraries[sys.platform]:
                 shutil.copy(os.path.join(root, name), bin_out)
             else:
