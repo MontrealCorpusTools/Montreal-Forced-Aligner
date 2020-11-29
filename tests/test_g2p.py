0906e618a8926d01b738d317510d6b201ea4107e
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2020-11-29 00:10:27 -0800

    Fix import errors

--- tests/test_g2p.py
+++ tests/test_g2p.py
@@ -1,8 +1,8 @@
 import pytest
 import os
-from montreal_forced_aligner.g2p.trainer import PhonetisaurusTrainer, PyniniTrainer
+from montreal_forced_aligner.g2p.trainer import PyniniTrainer
 
-from montreal_forced_aligner.g2p.generator import PhonetisaurusDictionaryGenerator
+from montreal_forced_aligner.g2p.generator import PyniniDictionaryGenerator
 
 from montreal_forced_aligner.models import G2PModel
 
@@ -12,18 +12,16 @@ from montreal_forced_aligner import __version__
 def test_training(sick_dict, sick_g2p_model_path, temp_dir):
     trainer = PyniniTrainer(sick_dict, sick_g2p_model_path, temp_directory=temp_dir)
     trainer.validate()
-    error
 
     trainer.train()
-    error
     model = G2PModel(sick_g2p_model_path)
     assert model.meta['version'] == __version__
-    assert model.meta['architecture'] == 'phonetisaurus'
+    assert model.meta['architecture'] == 'pynini'
     assert model.meta['phones'] == sick_dict.nonsil_phones
 
 
 def test_generator(sick_g2p_model_path, sick_corpus, g2p_sick_output):
     model = G2PModel(sick_g2p_model_path)
-    gen = PhonetisaurusDictionaryGenerator(model, sick_corpus.word_set)
+    gen = PyniniDictionaryGenerator(model, sick_corpus.word_set)
     gen.output(g2p_sick_output)
     assert os.path.exists(g2p_sick_output)
