7504e286dfc747afeccdc8f27d25885901c147df
Paweł Potrykus <52150545+potipot@users.noreply.github.com>
2021-02-25 03:00:54 +0100

    Fix char indexing (#235)
    Resolves #234

--- tests/test_g2p.py
+++ tests/test_g2p.py
@@ -17,6 +17,13 @@ def test_clean_up_word():
     assert m == ['+']
 
 
+def test_check_bracketed():
+    """Checks if the brackets are removed correctly and handling an empty string works"""
+    word_set = ['uh',  '(the)', 'sick', '<corpus>', '[a]', '{cold}', '']
+    expected_result = ['uh', 'sick', '']
+    assert [x for x in word_set if not check_bracketed(x)] == expected_result
+
+
 def test_training(sick_dict, sick_g2p_model_path, temp_dir):
     if G2P_DISABLED:
         pytest.skip('No Pynini found')
