9992a80b8d3fe8735583165e96dd248901cde92d
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2021-07-12 23:11:47 -0700

    Fix distribution bug

--- setup.py
+++ setup.py
@@ -55,12 +55,18 @@ def read(rel_path):
 
 
 def get_version(rel_path):
+    delim = ' = '
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
+        if line.startswith('__ver_major__'):
+            major_version = line.split(delim)[1]
+        elif line.startswith('__ver_minor__'):
+            minor_version = line.split(delim)[1]
+        elif line.startswith('__ver_patch__'):
+            patch_version = line.split(delim)[1].replace("'", '')
+            break
     else:
         raise RuntimeError("Unable to find version string.")
+    return "{}.{}.{}".format(major_version, minor_version, patch_version)
 
 
 if __name__ == '__main__':
