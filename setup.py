ca32b7c585975eddc73fa50c3f24c3699393b6eb
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2021-07-12 22:45:15 -0700

    Create setup.cfg

--- setup.py
+++ setup.py
@@ -42,9 +42,7 @@ class PyTest(TestCommand):
 
 
 if __name__ == '__main__':
-    from montreal_forced_aligner import __version__
     setup(name='Montreal Forced Aligner',
-          version=__version__,
           description='Montreal Forced Aligner is a package for aligning speech corpora through the use of '
                       'acoustic models and dictionaries using Kaldi functionality.',
           long_description=readme(),
