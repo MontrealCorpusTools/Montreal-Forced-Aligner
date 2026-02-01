f963335173c7bc1e372f434e05a1a54e2598f4df
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2026-02-01 01:24:45 -0800

    Update tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -88,13 +88,13 @@ def test_align_phones(basic_corpus_dir, basic_dict_path, temp_dir, eval_mapping_
         "ɹ",
     ]
     comparison_sequence = [CtmInterval(i, i + 1, x) for i, x in enumerate(comparison_sequence)]
-    score, phone_errors, error_counts = align_phones(
+    result = align_phones(
         reference_sequence,
         comparison_sequence,
-        silence_phone="sil",
+        silence_phones={"sil"},
         custom_mapping=mapping,
         debug=True,
     )
-
+    score, phone_errors, error_counts = result[:3]
     assert score < 1
     assert phone_errors < 1
